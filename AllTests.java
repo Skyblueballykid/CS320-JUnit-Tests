4b21b000da92947da82928c9b0ad5d4bd95f8b1a
Thomas Kalnik <kalnik.thomas@gmail.com>
2019-02-15 14:27:10 -0600

    Add files via upload

---
+++ AllTests.java
@@ -0,0 +1,11 @@
+package medical.com.medicalApplication.services;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DoctorServiceTest.class, MedicalRescordServiceTest.class })
+public class AllTests {
+
+}
